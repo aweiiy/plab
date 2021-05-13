300c9f6ec1493a8e2ae99d343be1a69ed186c9e0
aweiiy <e.sarauskas1@gmail.com>
2021-05-14 01:01:52 +0300

    Initial commit

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'PLAB store')
+@section('title', 'PLAB')
 
 @section('content')
 
@@ -8,8 +8,8 @@
         <div class="container pt-4 mb-3 mb-lg-0">
             <div class="row">
                 <div class="col-lg-5 col-md-6 col-sm-8">
-                    <h5 class="text-light fw-normal">New game store PLAB</h5>
-                    <h1 class="text-light mb-5">Enjoy games like never before</h1>
+                    <h5 class="text-light fw-normal">New game reviewing site - PLAB</h5>
+                    <h1 class="text-light mb-5">Review games like never before</h1>
                     <div class="d-flex align-items-center mb-5"><a class="btn btn-primary me-grid-gutter" href="/games"><i class="ci-cart fs-lg me-2"></i>Browse games</a></div>
                 </div>
             </div>
@@ -19,7 +19,7 @@
     <section>
         <div col-lg-5 col-md-6 col-sm-8 >
         <div class="container pt-4 mb-3 mb-lg-0" style="text-align: center">
-            <p style="font-size: 20px">Browse the latest games and add them to your wishlist</p>
+            <p style="font-size: 20px">Browse the latest game reviews</p>
         </div>
         </div>
     </section>
